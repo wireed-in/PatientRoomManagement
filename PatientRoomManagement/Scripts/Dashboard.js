c11336d857defdeecfdecf2997dc5ab1c9cf4823
Balagardash Sultanov <bsultanov@transplantconnect.com>
2016-02-23 15:51:26 -0800

    Addign dashboard page with typescript/jquery

---
+++ PatientRoomManagement/Scripts/Dashboard.js
@@ -0,0 +1,12 @@
+/// <reference path="typings/jquery.ui.layout/jquery.ui.layout.d.ts" />
+function dragAndDrop(draggableClass, dropableClass) {
+    var draggableBlock = $(draggableClass);
+    draggableBlock.draggable({ revert: "invalid" });
+    var droppableBlock = $(dropableClass);
+    droppableBlock.droppable({ hoverClass: "ui-state-active" });
+    droppableBlock.on("drop", function (event, ui) {
+        $(this).addClass("ui-state-highlight");
+    });
+}
+dragAndDrop(".patient-block", ".room-block");
+//# sourceMappingURL=Dashboard.js.map
---
+++ PatientRoomManagement/Scripts/Dashboard.ts
@@ -0,0 +1,19 @@
+﻿/// <reference path="typings/jquery.ui.layout/jquery.ui.layout.d.ts" />
+
+function dragAndDrop(draggableClass, dropableClass) {
+
+    var draggableBlock = $(draggableClass);
+
+    draggableBlock.draggable({ revert: "invalid" });
+
+    var droppableBlock = $(dropableClass);
+
+    droppableBlock.droppable({ hoverClass: "ui-state-active" });
+
+    droppableBlock.on("drop", function(event, ui) {
+        $(this).addClass("ui-state-highlight");
+    });
+
+}
+
+dragAndDrop(".patient-block", ".room-block");
