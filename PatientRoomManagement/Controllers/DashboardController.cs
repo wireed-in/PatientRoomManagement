9abb032f05219eb1896f4ec3a5c489a49387006f
Balagardash Sultanov <bsultanov@transplantconnect.com>
2016-02-22 21:23:29 -0800

    Adding Dashboard page

---
+++ PatientRoomManagement/Controllers/DashboardController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PatientRoomManagement.DataLayer;
+using PatientRoomManagement.ViewModels;
+using WebGrease.Css.Extensions;
+
+namespace PatientRoomManagement.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Dashboard
+        public ActionResult Index()
+        {
+            var unassignedPatients = db.Patients.Where(p => p.Assignments.Count == 0 || p.Assignments.All(a => a.SignOutDate.HasValue));
+
+            var availableRooms = db.Rooms.Where(r => r.Assignments.Count == 0 || r.NumberOfBeds > r.Assignments.Count(a => !a.SignOutDate.HasValue));
+
+            var possibleAssignments = new DashboardViewModel()
+            {
+                Rooms = availableRooms.ToList(),
+                Patients = unassignedPatients.ToList()
+            };
+
+            return View(possibleAssignments);
+        }
+    }
+}
---
+++ PatientRoomManagement/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PatientRoomManagement.Models;
+
+namespace PatientRoomManagement.ViewModels
+{
+    public class DashboardViewModel
+    {
+        public int PatientId { get; set; }
+        public int RoomId { get; set; }
+        public ICollection<Room> Rooms { get; set; }
+        public ICollection<Patient> Patients { get; set; }
+    }
+}
